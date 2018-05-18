3d7d83f535cf6886d953cc8fcd70897dfeebbb2c
Julian Simioni <julian@simioni.org>
2018-05-18 20:38:42 +0000

    Add stats, socket limit, more

--- src/searchStream.js
+++ src/searchStream.js
@@ -1,15 +1,33 @@
 var util = require('util');
 var through = require('through2');
 var request = require('request');
+const https = require('http');
 
+const agent = new https.Agent({ keepAlive: true, maxSockets: 80});
+
+
+const start = new Date();
+const stats = {
+  processed: 0,
+  error: 0,
+  zero_results: 0,
+  address: 0,
+  venue: 0,
+  admin: 0,
+  inFlightCount: 0
+};
 
 function createSearchStream(queryParams) {
+
 
   if (!queryParams.hasOwnProperty('api_key')) {
     throw new Error('Query parameters must at least contain api_key');
   }
 
-  var inFlightCount = 0;
+  const statInterval = setInterval(function() {
+    stats.RPS = stats.processed / ( new Date() - start) * 1000;
+    console.log(JSON.stringify(stats));
+  }, 1 * 1000);
 
   return through.obj(function (data, enc, next) {
     if (!data.hasOwnProperty('address')) {
@@ -18,21 +36,34 @@ function createSearchStream(queryParams) {
 
     var self = this;
 
-    inFlightCount++;
+    stats.inFlightCount++;
 
-    queryParams.text = data.address;
+
+    const this_query_params = {
+    };
+    this_query_params.text = data.name + ' ' + data.address;
 
     var reqOptions = {
-      url: 'https://search.mapzen.com/v1/search',
+      url: 'http://internal-a56c85a765acc11e8bebb0e73ead8d1c-196936346.us-east-1.elb.amazonaws.com:3100/v1/search',
       method: 'GET',
-      qs: queryParams
+      qs: this_query_params,
+      agent: agent
     };
 
     request(reqOptions, function (err, res, body) {
+      stats.processed++;
 
       if (err || res.statusCode !== 200) {
+        console.log(`${res.statusCode}: ${err ? err.message : res.statusMessage}`);
+
+        if (res.statusCode === 400) {
+          console.log(JSON.stringify(queryParams));
+          var resData = JSON.parse(body);
+          console.log(JSON.stringify(resData.geocoding.errors));
+        }
         self.push(addErrorData(data, (err ? err.message : res.statusMessage)));
-        inFlightCount--;
+        stats.error++;
+        stats.inFlightCount--;
         return;
       }
 
@@ -40,25 +71,32 @@ function createSearchStream(queryParams) {
 
       if (resData.length === 0) {
         self.push(addErrorData(data, '0 results'));
-        inFlightCount--;
+        stats.zero_results++;
+        stats.inFlightCount--;
         return;
       }
 
       self.push(addResData(data, resData));
-      inFlightCount--;
+      stats.inFlightCount--;
     });
 
-    // respect the rate limit and don't push another request sooner than 6 seconds
-    setTimeout(function () {
-      next(null);
-    }, 1000 / 5);
+    // adaptively adjust how quickly we progress through the stream
+    const nextInterval = setInterval(function () {
+      if (stats.inFlightCount > 200) {
+        return;
+      } else {
+        clearInterval(nextInterval)
+        next(null);
+      }
+    }, 10);
 
   },
   // don't flush the stream until the last in flight request has been handled
   function (done) {
     var interval = setInterval(function () {
-      if (inFlightCount === 0) {
+      if (stats.inFlightCount === 0) {
         clearInterval(interval);
+        clearInterval(statInterval);
         done();
       }
     }, 100);
@@ -78,6 +116,16 @@ function addResData(data, resData) {
   data.res_latitude = resData[0].geometry.coordinates[1];
   data.res_confidence = resData[0].properties.confidence;
   data.res_label = resData[0].properties.label;
+
+  //stats
+  if (resData[0].properties.layer == 'address') {
+    stats.address++;
+  } else if (resData[0].properties.layer == 'venue') {
+    stats.venue++;
+  } else {
+    stats.admin++;
+  }
+
   return data;
 }
 
