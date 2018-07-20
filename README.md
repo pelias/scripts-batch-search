63482efdcaf52f7c6e7e5d5f88e953e92f7f8c67
Julian Simioni <julian@simioni.org>
2018-07-20 12:59:36 -0400

    Improve configurability, reliability, etc

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Batch geocoder
+# Pelias Batch geocoder
 
-This is a basic utility for geocoding CSV files that have address data included. It will parse the file and add coordinate information, which is able to be customized with a variety of input parameters (discussed below). This tool is powered by Pelias, an experimental, community-focused geocoder from Mapzen, which is available for both commercial and non-commercial purposes. The [source code](https://github.com/pelias/pelias) is open to view and change, and contributions are welcomed.
+This is a basic utility for geocoding CSV files that have address data included. It will parse the file and add coordinate information, which is able to be customized with a variety of input parameters (discussed below). This tool is powered by the [Pelias geocoder](https://github.com/pelias/pelias).
 
 In order to use this tool, you’ll need some knowledge about command line (enough to be able to install node modules and run a script). _New to this?_ There are some [great tutorials available online](https://www.learnenough.com/command-line-tutorial).
 
@@ -12,9 +12,7 @@ This geocoder requires node.js and npm (which is included in node installs) to r
 For the time being, this geocoder only uses CSV files for input and output.
 
 ### Instructions
-1. In order to run this geocoder, you’re going to need to sign up for a [Mapzen API key](https://mapzen.com/developers)
-
-2. Clone this repository and install the node module by entering this code into your command line:
+Clone this repository and install the node module by entering this code into your command line:
 
 ```bash
 git clone https://github.com/pelias/scripts-batch-search.git
@@ -26,15 +24,24 @@ npm install
 
 4. Create a blank file named output.CSV. This is where the geocoded output will go.
 
-5. Create a JSON file called ‘parameters.json’. This is the query parameters document that will include your Mapzen API key and other search parameters you might want to include. Here’s an example of parameters:
+5. Create a JSON file called ‘parameters.json’. This is the query parameters document that will include your API key (if using a hosted instance of Pelias) and other search parameters you might want to include. Here’s an example of parameters:
 
 ```javascript
-{  
+{
   "api_key": "search-xxxxxx",
   "boundary.country": "USA",
   "sources": "openaddresses"
 }
 ```
+
+6. Configure the required, any any optional, environment variables:
+
+```bash
+export URL="https://api.geocode.earth" # or your own Pelias installation
+export MAX_IN_FLIGHT=10 # or other integer, if you want things to go faster
+export DELIMITER=',' # if you need to change the delimiter
+```
+
 
 *Don't set the `text` parameter in the queryParams file, because that will be overwritten with each address in the
 input data file.*
@@ -45,7 +52,7 @@ input data file.*
 npm run start -- ./file/input.csv ./file/output.csv ./file/parameters.json
 ```
 
-Once running, the script will update to let you know its progress through the file. Once finished, you should see your output CSV file contain all the data from the input CSV, plus additional columns representing the results. All results columns will be prefixed with res_ to avoid conflicts. You can see an example of expected output [here](test/expectedOutput.csv)
+Once running, the script will update to let you know its progress through the file. Once finished, you should see your output CSV file contain all the data from the input CSV, plus additional columns representing the results. All results columns will be prefixed with res\_ to avoid conflicts. You can see an example of expected output [here](test/expectedOutput.csv)
 
 ## Questions?
-Questions? Comments? Drop us a line: open a [GitHub issue](https://github.com/pelias/pelias/issues), or write us at [search@mapzen.com](mailto:search@mapzen.com)
+Questions? Comments? Drop us a line: open a [GitHub issue](https://github.com/pelias/pelias/issues).
--- src/searchStream.js
+++ src/searchStream.js
@@ -1,10 +1,18 @@
 var util = require('util');
 var through = require('through2');
-var request = require('request');
-const https = require('http');
+var request = require('requestretry');
+const http = require('http');
+const https = require('https');
 
-const agent = new https.Agent({ keepAlive: true, maxSockets: 80});
+// Base url of service
+const url = process.env.URL + '/v1/search';
+// maximum number of HTTP requests in flight
+const max_in_flight = process.env.MAX_IN_FLIGHT || 1;
+// seconds in between printing of statistics
+const stat_delay = process.env.STAT_DELAY || 1;
 
+const httpLib = url.startsWith('https:') ? https : http;
+const agent = new httpLib.Agent({ keepAlive: true, maxSockets: max_in_flight});
 
 const start = new Date();
 const stats = {
@@ -13,55 +21,71 @@ const stats = {
   zero_results: 0,
   address: 0,
   venue: 0,
+  street: 0,
   admin: 0,
   inFlightCount: 0
 };
+let last_progress = 0;
 
 function createSearchStream(queryParams) {
-
-
-  if (!queryParams.hasOwnProperty('api_key')) {
-    throw new Error('Query parameters must at least contain api_key');
-  }
-
   const statInterval = setInterval(function() {
-    stats.RPS = stats.processed / ( new Date() - start) * 1000;
+    stats.RPS = (stats.processed - last_progress) / stat_delay;
     console.log(JSON.stringify(stats));
-  }, 1 * 1000);
+    last_progress = stats.processed;
+  }, stat_delay * 1000);
 
   return through.obj(function (data, enc, next) {
     if (!data.hasOwnProperty('address')) {
+      console.log(data);
       return next(new Error('no address column'));
     }
 
     var self = this;
-
     stats.inFlightCount++;
 
+    const this_query_params = { };
+    this_query_params.text = data.address;
+    if (queryParams.api_key) {
+      this_query_params.api_key = queryParams.api_key;
+    }
 
-    const this_query_params = {
-    };
-    this_query_params.text = data.name + ' ' + data.address;
+    if (!data.address || data.address.length === 0) {
+      next();
+    }
 
     var reqOptions = {
-      url: 'http://internal-a56c85a765acc11e8bebb0e73ead8d1c-196936346.us-east-1.elb.amazonaws.com:3100/v1/search',
+      url: url,
       method: 'GET',
+      maxAttempts: 10,
+      retryDelay: 5000,
+      timeout: 10000,
       qs: this_query_params,
       agent: agent
     };
+
+    if (this_query_params.text.length > 300) {
+      console.log("warning, text over 100 length");
+      console.log(this_query_params.text);
+    }
 
     request(reqOptions, function (err, res, body) {
       stats.processed++;
 
       if (err || res.statusCode !== 200) {
-        console.log(`${res.statusCode}: ${err ? err.message : res.statusMessage}`);
-
-        if (res.statusCode === 400) {
-          console.log(JSON.stringify(queryParams));
-          var resData = JSON.parse(body);
-          console.log(JSON.stringify(resData.geocoding.errors));
+        if (res) {
+          console.log(`${res.statusCode}: ${err ? err.message : res.statusMessage}`);
+          if (res.statusCode === 400) {
+            console.log(JSON.stringify(this_query_params));
+            var resData = JSON.parse(body);
+            console.log(JSON.stringify(resData.geocoding.errors));
+          }
+          self.push(addErrorData(data, (err ? err.message : res.statusMessage)));
+        } else {
+          console.log("encoutered error with no no res: " + err.message);
+          console.log("query params: " + JSON.stringify(this_query_params));
+          self.push(addErrorData(data, (err ? err.message : res.statusMessage)));
         }
-        self.push(addErrorData(data, (err ? err.message : res.statusMessage)));
+
         stats.error++;
         stats.inFlightCount--;
         return;
@@ -81,15 +105,16 @@ function createSearchStream(queryParams) {
     });
 
     // adaptively adjust how quickly we progress through the stream
+    // essentially this is a busy-wait before the call to next()
+    const delay_until_next_attempt_at_return = 1000 / max_in_flight * 2;
     const nextInterval = setInterval(function () {
-      if (stats.inFlightCount > 200) {
+      if (stats.inFlightCount > max_in_flight / 2) {
         return;
       } else {
         clearInterval(nextInterval)
         next(null);
       }
-    }, 10);
-
+    }, delay_until_next_attempt_at_return);
   },
   // don't flush the stream until the last in flight request has been handled
   function (done) {
@@ -116,12 +141,15 @@ function addResData(data, resData) {
   data.res_latitude = resData[0].geometry.coordinates[1];
   data.res_confidence = resData[0].properties.confidence;
   data.res_label = resData[0].properties.label;
+  data.res_layer = resData[0].properties.layer;
 
   //stats
   if (resData[0].properties.layer == 'address') {
     stats.address++;
   } else if (resData[0].properties.layer == 'venue') {
     stats.venue++;
+  } else if (resData[0].properties.layer == 'street') {
+    stats.street++;
   } else {
     stats.admin++;
   }
